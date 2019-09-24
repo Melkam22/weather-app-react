193b4616f74989fafd0445ea64f9ea4d88d70ca9
Ashenafi <asheaddis@yahoo.com>
2019-09-24 22:13:10 +0200

    Updates

---
+++ precache-manifest.5408fa7faa8f1311610fbb5f73781bce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1a01de79a70b703850088f05711f9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc5ca82aa402ad544bf5",
+    "url": "./static/css/main.9a107cd1.chunk.css"
+  },
+  {
+    "revision": "2bb8f3a90fd2c4ab9e51",
+    "url": "./static/js/2.aa49769e.chunk.js"
+  },
+  {
+    "revision": "cc5ca82aa402ad544bf5",
+    "url": "./static/js/main.5f0761a4.chunk.js"
+  },
+  {
+    "revision": "60bcdcea711194154b1c",
+    "url": "./static/js/runtime~main.880125e3.js"
+  },
+  {
+    "revision": "ca4d3ab04f9a33ed639d245e8dbd653d",
+    "url": "./static/media/drizzle.ca4d3ab0.jpeg"
+  },
+  {
+    "revision": "c08c607a7f8aebba025b0c316890067b",
+    "url": "./static/media/rain.c08c607a.jpeg"
+  },
+  {
+    "revision": "56e27131144146b2672c792585e2e6d4",
+    "url": "./static/media/scateredclouds.56e27131.jpeg"
+  },
+  {
+    "revision": "416a4065698f5e94933f3e531f74539f",
+    "url": "./static/media/sunflower.416a4065.jpeg"
+  },
+  {
+    "revision": "d5b737608d49e0e2995b099f8d07cba1",
+    "url": "./static/media/wind.d5b73760.jpg"
+  }
+]);
